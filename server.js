93e2d90dad4fd49465817e379ef700434115e2f6
ekmy318 <eunice.xp@gmail.com>
2019-08-12 11:57:14 -0400

    Delete console.log
    Co-Authored-By: elody-ramirez <elody-ramirez@users.noreply.github.com>

--- server.js
+++ server.js
@@ -74,12 +74,9 @@ const server = app.listen(port)
 
 const io = require('socket.io').listen(server)
 io.on('connection', function (socket) {
-  console.log('a user connected')
   socket.on('disconnect', function () {
-    console.log('user disconnected')
   })
   socket.on('chat message', function (msg) {
-    console.log('message: ' + msg)
   })
   socket.on('chat message', function (msg) {
     io.emit('chat message', msg)
